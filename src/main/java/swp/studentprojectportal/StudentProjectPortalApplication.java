49fa320807b7400f5d7a5d8ede67cde36b796709
Minh Duc <ngominhduc100@gmail.com>
2023-09-13 10:38:56 +0700

    git init

---
+++ src/main/java/swp/studentprojectportal/StudentProjectPortalApplication.java
@@ -0,0 +1,13 @@
+package swp.studentprojectportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProjectPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentProjectPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swp/studentprojectportal/StudentProjectPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package swp.studentprojectportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentProjectPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
