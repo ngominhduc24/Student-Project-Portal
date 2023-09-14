d5942ff703417c536eb71bd8e78fce8af698db8e
maidung1602 <maidung1602@gmail.com>
2023-09-14 22:39:23 +0700

    merge conflict mduc

--- src/main/java/swp/studentprojectportal/StudentProjectPortalApplication.java
+++ src/main/java/swp/studentprojectportal/StudentProjectPortalApplication.java
@@ -2,12 +2,19 @@ package swp.studentprojectportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import swp.studentprojectportal.repository.IUserRepository;
 
 @SpringBootApplication
 public class StudentProjectPortalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentProjectPortalApplication.class, args);
+        ApplicationContext context = SpringApplication.run(StudentProjectPortalApplication.class, args);
+        IUserRepository userRepository = context.getBean(IUserRepository.class);
+
+        // Lấy ra toàn bộ user trong db
+        userRepository.findAll()
+                .forEach(System.out::println);
     }
 
 }
