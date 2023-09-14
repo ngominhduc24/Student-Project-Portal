aff427948c29ee1a57b0c7cc2c6c88c5bc3e16a0
Minh Duc <ngominhduc100@gmail.com>
2023-09-14 23:00:30 +0700

    merge branch dung

--- src/main/java/swp/studentprojectportal/StudentProjectPortalApplication.java
+++ src/main/java/swp/studentprojectportal/StudentProjectPortalApplication.java
@@ -10,12 +10,7 @@ import swp.studentprojectportal.repository.IUserRepository;
 public class StudentProjectPortalApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StudentProjectPortalApplication.class, args);
-        IUserRepository userRepository = context.getBean(IUserRepository.class);
-
-        // Lấy ra toàn bộ user trong db
-        User u = userRepository.findUserByEmailAndPassword("gillianmorris@gmail.com", "123456");
-        System.out.println(u);
+        SpringApplication.run(StudentProjectPortalApplication.class, args);
     }
 
 }
