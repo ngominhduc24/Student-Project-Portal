e03a5f02cf539d5efd789a48029cb3ba06ddfba3
maidung1602 <maidung1602@gmail.com>
2023-09-14 22:54:08 +0700

    Feat: login

--- src/main/java/swp/studentprojectportal/controller/loginController.java
+++ src/main/java/swp/studentprojectportal/controller/loginController.java
@@ -1,12 +1,40 @@
 package swp.studentprojectportal.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import swp.studentprojectportal.model.User;
+import swp.studentprojectportal.services.servicesimpl.UserService;
 
 @Controller
 public class loginController {
+    @Autowired
+    UserService userService;
     @RequestMapping("/login")
     public String loginPage() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String userLogin(WebRequest request, Model model, HttpSession session) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user;
+        if (username.contains("@"))
+            user = userService.findUserByEmailAndPassword(username, password);
+        else
+            user = userService.findUserByPhoneAndPassword(username, password);
+        if(user != null) {
+            session.setAttribute("user", user);
+            return "redirect:/";
+        }
+        else {
+            model.addAttribute("errmsg", "username or password is not correct");
+            return "login";
+        }
     }
 }
