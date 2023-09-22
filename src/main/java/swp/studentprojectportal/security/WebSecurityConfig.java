50789ecf3050e46659a3000be6abc418afe9431e
Minh Duc <ngominhduc100@gmail.com>
2023-09-22 16:50:31 +0700

    add file spring security (not config)

--- src/main/java/swp/studentprojectportal/security/WebSecurityConfig.java
+++ src/main/java/swp/studentprojectportal/security/WebSecurityConfig.java
@@ -19,41 +19,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    @Autowired
-    CustomUserService userService;
-    @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.isConfigured();
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/register", "forgotPassword").permitAll()
-                        .requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**","/vendor/**","/fonts/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/profile")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
+                        .requestMatchers("/**")
                         .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+                );
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/", "/home", "/register", "forgotPassword").permitAll()
+//                        .requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**","/vendor/**","/fonts/**").permitAll()
+//                );
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/profile")
+//                        .usernameParameter("username")
+//                        .passwordParameter("password")
+//                        .permitAll()
+//                )
+//                .logout((logout) -> logout.permitAll());
 
         return http.build();
-    }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
