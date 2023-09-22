391bbcb24803269851034ad075489aa1a23c7f9a
NTN <thanhdeapool@gmail.com>
2023-09-23 03:07:58 +0700

    fix SubjectSettingController + subjectSettingList.html + create test.html

--- src/main/java/swp/studentprojectportal/controller/subject_manager/SubjectSettingController.java
+++ src/main/java/swp/studentprojectportal/controller/subject_manager/SubjectSettingController.java
@@ -2,10 +2,13 @@ package swp.studentprojectportal.controller.subject_manager;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 import swp.studentprojectportal.model.Setting;
 import swp.studentprojectportal.model.Subject;
 import swp.studentprojectportal.model.SubjectSetting;
@@ -40,4 +43,23 @@ public class SubjectSettingController {
         subjectSettingService.saveSubjectSetting(subjectSetting);
         return "redirect:/";
     }
+    @RequestMapping("/subject-manager/subject-setting/detail")
+    public String updateSubjectSetting(@RequestParam("settingID") int id, Model model, WebRequest request){
+
+//        Integer id = Integer.parseInt( request.getParameter("id") );
+        System.out.println("id="+id);
+        System.out.println("noway");
+//        SubjectSetting subjectSetting = subjectSettingService.findById(id);
+//        model.addAttribute("setting",subjectSetting);
+//        return "subject_manager/subject_setting/subjectSettingDetail";
+        return "redirect:/profile";
+    }
+
+    @RequestMapping(path="/test")
+    public  String testNTN(@RequestParam("settingID") int settingId){
+        System.out.println("hehe "+settingId);
+        return "redirect:/profile";
+    }
+
+
 }
