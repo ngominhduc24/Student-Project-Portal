e6518345c0e25d9897ecfe7a0f904a6ac9ec5d5e
NTN <thanhdeapool@gmail.com>
2023-09-26 01:05:52 +0700

    fix add new subject setting

--- src/main/java/swp/studentprojectportal/controller/subject_manager/SubjectSettingController.java
+++ src/main/java/swp/studentprojectportal/controller/subject_manager/SubjectSettingController.java
@@ -2,7 +2,6 @@ package swp.studentprojectportal.controller.subject_manager;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
-import swp.studentprojectportal.model.Setting;
 import swp.studentprojectportal.model.Subject;
 import swp.studentprojectportal.model.SubjectSetting;
 import swp.studentprojectportal.model.User;
@@ -78,13 +76,28 @@ public class SubjectSettingController {
         return "test";
     }
     @RequestMapping(path = "/subject-manager/subject-setting/add")
-    public String addSubjectSetting(){
-
+    public String addSubjectSettingaPage(WebRequest request,HttpSession session){
+        User user = (User) session.getAttribute("user");
+        Subject subject = subjectRepository.findSubjectByUser(user);
+        String type = request.getParameter("type");
+        String title = request.getParameter("title");
+        String displayOrder = request.getParameter("displayOrder");
+        SubjectSetting subjectSetting = new SubjectSetting();
+        subjectSetting.setTypeId(Integer.parseInt(type));
+        subjectSetting.setSettingTitle(title);
+        subjectSetting.setDisplayOrder(Integer.parseInt(displayOrder));
+        subjectSetting.setStatus(true);
+        subjectSetting.setSubject(subject);
+        subjectSettingService.saveSubjectSetting(subjectSetting);
+        return  "redirect:/subject-manager/subject-setting";
     }
+//    @PostMapping(path="/subject-manager/subject-setting/add")
+//    public  String addSubjectSeting()Ơ
+
     @RequestMapping(path="/test")
-    public  String testNTN(@RequestParam("settingID") int settingId){
-        System.out.println("hehe "+settingId);
-        return "redirect:/profile";
+    public  String testNTN(){
+        //System.out.println("ntn00"+id);
+        return "subject_manager/subject_setting/addSubjectSetting";
     }
 
 
