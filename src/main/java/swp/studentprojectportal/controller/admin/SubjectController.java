f19c5cfadf5c566fcccf8f7ae3926166c7e25c74
daylapvlong <101397074+daylapvlong@users.noreply.github.com>
2023-09-26 09:38:29 +0700

    minor fix on subject add

--- src/main/java/swp/studentprojectportal/controller/admin/SubjectController.java
+++ src/main/java/swp/studentprojectportal/controller/admin/SubjectController.java
@@ -21,12 +21,6 @@ public class SubjectController {
     UserService userService;
 
     List<Subject> subjectList = new CopyOnWriteArrayList<>();
-    private boolean isSubjectAdded(String subjectName, String subjectCode, int subjectManagerId) {
-        if(subjectName == null || subjectCode == null || subjectManagerId == 0) {
-            return false;
-        }
-        return true;
-    }
 
     @GetMapping("/admin/subject")
     public String subjectPage(Model model) {
@@ -99,6 +93,8 @@ public class SubjectController {
     }
 
     private String checkValidate(String subjectName, String subjectCode) {
+        if(subjectName.isEmpty()) return "Please input subject name";
+        if(subjectCode.isEmpty()) return "Please input subject code";
 
         if(subjectService.checkSubjectNameExist(subjectName)) return "Subject name already exist";
         if(subjectService.checkSubjectCodeExist(subjectCode)) return "Subject code already exist";
