5d225e83973c1c6d20307007f5f448735f3b4bfb
Minh Duc <ngominhduc100@gmail.com>
2023-09-26 12:01:45 +0700

    update html for mail sender

--- src/main/java/swp/studentprojectportal/service/servicesimpl/EmailService.java
+++ src/main/java/swp/studentprojectportal/service/servicesimpl/EmailService.java
@@ -1,28 +1,85 @@
 package swp.studentprojectportal.service.servicesimpl;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import swp.studentprojectportal.service.IEmailService;
+
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class EmailService implements IEmailService {
     @Autowired
     private JavaMailSender mailSender;
+
     @Override
     public void sendEmail(String name, String to, String token) {
-        String body = "\n" +
-                "Hi " + name + "\n" +
+        String fromAddress = "Studix@sss";
+        String senderName = "Studix";
+        String subject = "Activate your Studix account";
+        String content1 = "Dear [[name]],<br>"
+                +"Your email has been used to register Studix<br>"
+                + "Please click the link below to verify your registration:<br>"
+                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3><br>"
+                + "If you did not do this, please click here:<br>"
+                + "<h3><a href=\"[[URL_discard]]\" target=\"_self\">DISCARD</a></h3><br>"
+                + "Thank you,<br>"
+                + "Studix.";
+        String content = "\n" +
+                "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
                 "\n" +
-                "You registered an account on SPP-G3, before being able to use your account you need to verify that this is your email address by clicking here: " + token;
-        String subject = "Activate your SPP-G3 account";
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("G3@sss");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+                "    <head>\n" +
+                "        <meta charset=\"UTF-8\">\n" +
+                "        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "        <title>Tailwind CSS Simple Email Template Example </title>\n" +
+                "        <link href=\"https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css\" rel=\"stylesheet\">\n" +
+                "\n" +
+                "    </head>\n" +
+                "\n" +
+                "    <body>\n" +
+                "        <div class=\"flex items-center justify-center min-h-screen p-5 bg-gray-600 min-w-screen\">\n" +
+                "            <div class=\"max-w-xl p-8 text-center text-gray-800 bg-white shadow-xl lg:max-w-3xl rounded-3xl lg:p-12\">\n" +
+                "                <h3 class=\"text-2xl\">Thanks [[name]] for signing up for Studix!</h3>\n" +
+                "                <div class=\"flex justify-center\">\n" +
+                "                    <img style=\"height: 80px;\"\n" +
+                "                    src=\"https://res.cloudinary.com/dxu6nsoye/image/upload/v1695704345/logog3-removebg-preview_ptkuxj.png\"\n" +
+                "                    alt=\"\">\n" +
+                "                </div>\n" +
+                "                <p>We're happy you're here. Let's get your email address verified:</p>\n" +
+                "                <div class=\"mt-4\">\n" +
+                "                    <a href=\"[[URL]]\"> <button class=\"px-2 py-2 text-blue-200 bg-blue-600 rounded\">Click to Verify Email</button></a>\n" +
+                "                    <p class=\"mt-4 text-sm\">If you’re having trouble clicking the \"Verify Email Address\" button, copy and paste the URL below into your web browser: <br>\n" +
+                "                        <a href=\"\" class=\"text-blue-600\">[[URL]]</a>\n" +
+                "                    </p>\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "        </div>\n" +
+                "\n" +
+                "    </body>\n" +
+                "</html>";
+        MimeMessage message = mailSender.createMimeMessage();
+         try {
+            content = content.replace("[[name]]", name);
+            content = content.replace("[[URL]]", token);
 
-        mailSender.send(message);
+            message.setFrom(fromAddress);
+            message.setRecipients(MimeMessage.RecipientType.TO, to);
+            message.setSubject(subject);
+            message.setContent(content, "text/html;charset=UTF-8");
+
+            mailSender.send(message);
+        } catch (MessagingException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
 }
