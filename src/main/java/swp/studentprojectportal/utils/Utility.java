a0a865fa43f8e769df0852b0c91f1636682c66e0
maidung1602 <maidung1602@gmail.com>
2023-09-28 16:13:55 +0700

    feat: hash function to Utility

--- src/main/java/swp/studentprojectportal/utils/Utility.java
+++ src/main/java/swp/studentprojectportal/utils/Utility.java
@@ -1,7 +1,11 @@
 package swp.studentprojectportal.utils;
 
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponents;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Utility {
     public static String getSiteURL() {
@@ -11,5 +15,12 @@ public class Utility {
         String siteUrl = components.toUriString();
         String path=components.getPath();
         return siteUrl.replace(path, "");
+    }
+
+    public static String hash(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        return Hex.encodeHexString(digest).toUpperCase();
     }
 }
