cefa4443bed621e181bfe71b66d154deb187a84a
Minh Duc <ngominhduc100@gmail.com>
2023-09-28 15:57:32 +0700

    feat: add filter role for studentlist

--- src/main/java/swp/studentprojectportal/repository/IUserRepository.java
+++ src/main/java/swp/studentprojectportal/repository/IUserRepository.java
@@ -3,6 +3,8 @@ package swp.studentprojectportal.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swp.studentprojectportal.model.Setting;
 import swp.studentprojectportal.model.User;
@@ -19,4 +21,12 @@ public interface IUserRepository extends JpaRepository<User, Integer> {
     User findUserByEmailOrPhone(String email, String phone);
     List<User> findAllBySetting(Setting setting);
     Page<User> findUserByFullNameContainsIgnoreCaseOrEmailContainsIgnoreCaseOrPhoneContainsIgnoreCase(String fullName, String email, String phone, Pageable pageable);
+@Query(value = "SELECT * FROM user u " +
+        "WHERE (LOWER(u.full_name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) " +
+        "OR LOWER(u.email) LIKE LOWER(CONCAT('%', :searchTerm, '%')) " +
+        "OR LOWER(u.phone) LIKE LOWER(CONCAT('%', :searchTerm, '%'))) " +
+        "AND u.role_id = :roleId",
+        nativeQuery = true)
+Page<User> searchUsersAndFilterByRole(@Param("searchTerm") String searchTerm, @Param("roleId") Integer roleId, Pageable pageable);
+
 }
