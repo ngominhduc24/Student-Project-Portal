58d690d7c5e4c24a8564730fb9418762ca1955b1
NTN <thanhdeapool@gmail.com>
2023-10-06 15:53:06 +0700

    fix pom.xml + profile.html

--- src/test/java/swp/studentprojectportal/StudentProjectPortalApplicationTests.java
+++ src/test/java/swp/studentprojectportal/StudentProjectPortalApplicationTests.java
@@ -2,12 +2,23 @@ package swp.studentprojectportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StudentProjectPortalApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testAddition() {
+        int result = add(2, 3);
+        assertEquals(6, result, "Expected the sum to be 5");
+    }
+
+    private int add(int a, int b) {
+        return a + b;
     }
 
 }
