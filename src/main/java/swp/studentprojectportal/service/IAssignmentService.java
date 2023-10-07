4274b75f99afb6441e5e711d652c73927133f566
NTN <thanhdeapool@gmail.com>
2023-10-07 19:39:14 +0700

    subjectassignment

--- src/main/java/swp/studentprojectportal/service/IAssignmentService.java
+++ src/main/java/swp/studentprojectportal/service/IAssignmentService.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface IAssignmentService {
     List<Assignment> findAllAssignment(Integer pageno, Integer pagesize);
+
+    List<Assignment> findAssignmentByManager(int subjectManagerId);
+
+    Assignment findById(int id);
+
+    Assignment saveAssignment(Assignment assignment);
 }
