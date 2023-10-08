7095cb4ba0e1ed00d40e37d36005c106e7d4fa89
maidung1602 <maidung1602@gmail.com>
2023-10-09 05:08:51 +0700

    fix: setting

--- src/main/java/swp/studentprojectportal/repository/ISettingRepository.java
+++ src/main/java/swp/studentprojectportal/repository/ISettingRepository.java
@@ -1,5 +1,7 @@
 package swp.studentprojectportal.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -17,4 +19,9 @@ public interface ISettingRepository extends JpaRepository<Setting, Integer> {
     @Query(value="SELECT distinct setting.* FROM class c join subject s on c.subject_id = s.id join setting on c.semester_id = setting.id\n" +
             "WHERE s.subject_manager_id = ?1", nativeQuery = true)
     List<Setting> findSemesterBySubjectManagerId(int subjectManagerId);
+    @Query(value="SELECT * FROM setting \n" +
+            "WHERE (LOWER(setting_title) LIKE LOWER(CONCAT('%', :search, '%'))  \n" +
+            "OR LOWER(display_order) LIKE LOWER(CONCAT('%', :search, '%'))) \n" +
+            "and (:typeId = -1 OR type_id = :typeId) and (:status = -1 OR status = :status)", nativeQuery = true)
+    Page<Setting> filter(String search, Integer typeId, Integer status, Pageable pageable);
 }
