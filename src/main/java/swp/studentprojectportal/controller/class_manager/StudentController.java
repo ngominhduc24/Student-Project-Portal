4775093e659eeaaea07b90eeda4a050197d6caf4
Minh Duc <ngominhduc100@gmail.com>
2023-10-09 10:05:34 +0700

    fix: student list

--- src/main/java/swp/studentprojectportal/controller/class_manager/StudentController.java
+++ src/main/java/swp/studentprojectportal/controller/class_manager/StudentController.java
@@ -1,6 +1,7 @@
 package swp.studentprojectportal.controller.class_manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import swp.studentprojectportal.model.User;
 import swp.studentprojectportal.service.servicesimpl.ClassService;
 import swp.studentprojectportal.model.Class;
 import swp.studentprojectportal.service.servicesimpl.SettingService;
+import swp.studentprojectportal.service.servicesimpl.StudentClassService;
 import swp.studentprojectportal.service.servicesimpl.UserService;
 
 import java.util.Optional;
@@ -23,17 +25,21 @@ public class StudentController {
     UserService userService;
     @Autowired
     SettingService settingService;
+    @Autowired
+    StudentClassService studentClassService;
 
     @GetMapping("/class")
     public String studentList(Model model,
                            @RequestParam(defaultValue = "-1") int classId) {
+        final int roleId = 1;
         Class c = classService.getClass(classId);
         if(c == null) {
             return "redirect:/class";
         }
+        model.addAttribute("classId", classId);
         model.addAttribute("className", c.getClassName());
         model.addAttribute("semester", c.getSemester().getSettingTitle());
-        model.addAttribute("totalPage", userService.getTotalPage(10, 1));
+        model.addAttribute("totalPage", userService.getTotalPage(10, roleId));
         model.addAttribute("studentList", classService.getAllStudent(classId));
         return "class_manager/student/studentList";
     }
@@ -45,5 +51,13 @@ public class StudentController {
         model.addAttribute("user", user.isPresent() ? user.get() : null);
         model.addAttribute("roleList", settingService.getAllRole());
         return "class_manager/student/studentDetails";
+    }
+
+    @GetMapping("/class/removeStudentFromClass")
+    public String removeStudentFromClass(
+            @RequestParam(name = "classId") Integer classId,
+            @RequestParam(name = "studentId") Integer studentId) {
+        boolean result =  studentClassService.removeStudentFromClass(classId, studentId);
+        return "redirect:/class-manager/class?classId=" + classId;
     }
 }
