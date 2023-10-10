f922f7b68cb0b341c384bf1fad20885005cf8688
NTN <thanhdeapool@gmail.com>
2023-10-10 23:52:03 +0700

    fix

--- src/main/java/swp/studentprojectportal/repository/IAssignmentRepository.java
+++ src/main/java/swp/studentprojectportal/repository/IAssignmentRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface IAssignmentRepository extends JpaRepository<Assignment, Integer> {
     @Query(value = "SELECT ass.* FROM assignment ass join subject s on ass.subject_id = s.id \n"+
-                    "WHERE s.subject_manager_id=?1\n"+
-                    "ORDER BY ass.subject_id,ass.title",nativeQuery = true)
+            "WHERE s.subject_manager_id=?1\n"+
+            "ORDER BY ass.subject_id,ass.title",nativeQuery = true)
     List<Assignment> findAssignmentByManager(int subjectManagerID);
 }
