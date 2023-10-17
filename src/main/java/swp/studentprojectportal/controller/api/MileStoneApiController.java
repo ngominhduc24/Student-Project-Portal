cf9bc838f01e21eee641248b7f2858ec89f9be7c
Minh Duc <ngominhduc100@gmail.com>
2023-10-17 11:23:37 +0700

    feat: api milestone detail

--- src/main/java/swp/studentprojectportal/controller/api/MileStoneApiController.java
+++ src/main/java/swp/studentprojectportal/controller/api/MileStoneApiController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import swp.studentprojectportal.model.Milestone;
 import swp.studentprojectportal.model.User;
 import swp.studentprojectportal.repository.IStudentClassRepository;
 import swp.studentprojectportal.service.servicesimpl.ClassService;
@@ -11,7 +12,9 @@ import swp.studentprojectportal.service.servicesimpl.MilestoneService;
 import swp.studentprojectportal.service.servicesimpl.StudentClassService;
 import swp.studentprojectportal.service.servicesimpl.UserService;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -33,5 +36,25 @@ public class MileStoneApiController {
                 return ResponseEntity.ok().body("Add new milestone successfully");
             else
                 return ResponseEntity.badRequest().body("Add new milestone failed");
+        }
+
+        @GetMapping("/milestone")
+        public ResponseEntity getMilestoneByClassId(@RequestParam(name = "milestoneId") Integer milestoneId) {
+            Milestone milestone = milestoneService.findMilestoneById(milestoneId);
+            // create response entity with milestone object
+            if(milestone != null){
+                Map<String, Object> response = new HashMap<>();
+                response.put("title", milestone.getTitle());
+                response.put("description", milestone.getDescription());
+                response.put("startDate", milestone.getStartDate().toString());
+                response.put("endDate", milestone.getEndDate().toString());
+                response.put("status", milestone.isStatus() == true ? "1" : "0");
+                response.put("classId", milestone.getAclass() == null ? "" : milestone.getAclass().getId());
+                response.put("projectId", milestone.getProject() == null ? "" : milestone.getProject().getId());
+                return ResponseEntity.ok().body(response);
+            }
+            else
+                return ResponseEntity.badRequest().body("Milestone not found");
+
         }
 }
