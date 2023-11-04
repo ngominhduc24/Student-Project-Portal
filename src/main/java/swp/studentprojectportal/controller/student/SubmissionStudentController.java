73e345edd9566b7fd902bb8619ea5720a6afabab
anhduc2337 <anhduc2337@gmail.com>
2023-11-04 15:15:44 +0700

    add submit details screen for project mentor and student

--- src/main/java/swp/studentprojectportal/controller/student/SubmissionStudentController.java
+++ src/main/java/swp/studentprojectportal/controller/student/SubmissionStudentController.java
@@ -1,4 +1,4 @@
-package swp.studentprojectportal.controller.project_mentor;
+package swp.studentprojectportal.controller.student;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import swp.studentprojectportal.model.Submission;
 import swp.studentprojectportal.model.User;
 import swp.studentprojectportal.service.servicesimpl.SubmissionService;
 import swp.studentprojectportal.service.servicesimpl.SubmitIssueService;
 
 @Controller
-@RequestMapping("/project-mentor/submission")
-public class SubmissionProjectMentorController {
+@RequestMapping("/student/submission")
+public class SubmissionStudentController {
     @Autowired
     SubmissionService submissionService;
     @Autowired
