322b9aa8d1ac16caf0492041e17d803770bc11eb
Minh Duc <ngominhduc100@gmail.com>
2023-11-11 22:19:15 +0700

    database: add gitlab id for milestone

--- src/main/java/swp/studentprojectportal/model/Milestone.java
+++ src/main/java/swp/studentprojectportal/model/Milestone.java
@@ -36,6 +36,9 @@ public class Milestone {
     @Column(name = "status")
     private boolean status = true;
 
+    @Column(name = "gitlab_milestone_id")
+    private Integer gitlabMilestoneId;
+
     @ManyToOne
     @JoinColumn(name = "subject_assignment_id")
     private Assignment subjectAssignment;
