75aeb9f507a7466eac6fdd7a15f63ef6efc2e071
anhduc2337 <anhduc2337@gmail.com>
2023-11-16 07:48:47 +0700

    fix student dashboard

--- src/main/java/swp/studentprojectportal/controller/student/StudentHomeController.java
+++ src/main/java/swp/studentprojectportal/controller/student/StudentHomeController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import swp.studentprojectportal.model.Issue;
 import swp.studentprojectportal.model.Milestone;
 import swp.studentprojectportal.model.Project;
 import swp.studentprojectportal.model.User;
+import swp.studentprojectportal.service.servicesimpl.IssueService;
 import swp.studentprojectportal.service.servicesimpl.MilestoneService;
 import swp.studentprojectportal.service.servicesimpl.ProjectService;
 import swp.studentprojectportal.service.servicesimpl.SettingService;
@@ -25,6 +27,8 @@ public class StudentHomeController {
     ProjectService projectService;
     @Autowired
     MilestoneService milestoneService;
+    @Autowired
+    IssueService issueService;
 
     @GetMapping("student/home")
     public String dashboard(@RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "6") Integer pageSize,
@@ -32,7 +36,14 @@ public class StudentHomeController {
         User user = (User) session.getAttribute("user");
         Page<Project> projectList = projectService.filterByStudendDashboard(user.getId(), pageNo, pageSize);
 
-        // bar
+        // donut issue
+        List<Issue> issueList = issueService.findAllByStudentId(user.getId());
+        int countAllIssue = issueList.size();
+        int countDoing = (int) issueList.stream().filter(i -> i.getStatus().getId()==3).count();
+        int countDone = (int) issueList.stream().filter(i -> i.getStatus().getId()==4).count();
+        int countToDo = (int) issueList.stream().filter(i -> i.getStatus().getId()==5).count();
+
+        // bar assignment
         List<String> barLabel = new ArrayList<>();
         List<Integer> barCount = new ArrayList<>();
 
@@ -41,7 +52,7 @@ public class StudentHomeController {
             barCount.add(milestoneService.findAllBySubjectAndClassOfProject(project.getAclass().getId()).size());
         }
 
-        // donut
+        // donut assignment
         List<Milestone> milestoneList = milestoneService.findAllByStudentId(user.getId());
         int countAllAssignment = milestoneList.size();
         int countPending = (int) milestoneList.stream().filter(m -> m.getSubmissionList().isEmpty()).count();
@@ -59,6 +70,11 @@ public class StudentHomeController {
         model.addAttribute("countAllAssignment", countAllAssignment);
         model.addAttribute("countPending", countPending);
         model.addAttribute("countSubmitted", countSubmitted);
+
+        model.addAttribute("countAllIssue", countAllAssignment);
+        model.addAttribute("countDoing", countDoing);
+        model.addAttribute("countDone", countDone);
+        model.addAttribute("countToDo", countToDo);
 
         return "student/studentDashboard";
     }
